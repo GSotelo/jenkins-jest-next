d0899522cc6fd7d55a7b8e8d9ba62d9e311c6523
gsotelo <gsotelo.se@gmail.com>
2025-02-23 10:19:05 +0100

    Add config
    Jest with TypeScript.

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+} satisfies Config
+
+export default createJestConfig(config)
