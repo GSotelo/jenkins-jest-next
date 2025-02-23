ecd066c1217b16249c6121659a18716359cc64bd
gsotelo <gsotelo.se@gmail.com>
2025-02-23 11:08:00 +0100

    Code style
    Format code.

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -2,11 +2,11 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
 describe('Page', () => {
-    it('renders a heading', () => {
-        render(<h1>HelloWorldTest</h1>)
+  it('renders a heading', () => {
+    render(<h1>HelloWorldTest</h1>)
 
-        const heading = screen.getByRole('heading', { level: 1 })
+    const heading = screen.getByRole('heading', { level: 1 })
 
-        expect(heading).toBeInTheDocument()
-    })
+    expect(heading).toBeInTheDocument()
+  })
 })
