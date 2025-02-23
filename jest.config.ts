0d8dd63746e5dd49ec0b4120d59ac2f3f4836a61
gsotelo <gsotelo.se@gmail.com>
2025-02-23 11:36:45 +0100

    Update config
    Jest.

--- jest.config.ts
+++ jest.config.ts
@@ -8,10 +8,24 @@ const createJestConfig = nextJest({
 const config = {
   clearMocks: true,
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  coverageReporters: ['text', 'lcov', 'html'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 } satisfies Config
 
 export default createJestConfig(config)
